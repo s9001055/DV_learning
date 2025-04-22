4e3f9798aaf80ccc295e7cea45ba0b39cd7b1019
Arthur Chang <s9001055@gmail.com>
2025-04-22 22:32:19 +0800

    Add: 1. Add struct example code 2. Add struct slide

---
+++ systemverilog/Data_Type/03_Structures/packed_struct.sv
@@ -0,0 +1,35 @@
+// A structure is declared packed using the packed keyword which by default is unsigned.
+// Create a "packed" structure data type which is similar to creating
+// bit [7:0]  ctrl_reg;
+// ctrl_reg [0]   represents en
+// ctrl_reg [3:1] represents cfg
+// ctrl_reg [7:4] represents mode
+typedef struct packed {
+  bit [3:0] mode;
+  bit [2:0] cfg;
+  bit       en;
+} st_ctrl;
+
+module tb;
+  st_ctrl    ctrl_reg;
+
+  initial begin
+    // Initialize packed structure variable
+    ctrl_reg = '{4'ha, 3'h5, 1};
+    $display ("ctrl_reg = %p", ctrl_reg);
+
+    // Change packed structure member to something else
+    ctrl_reg.mode = 4'h3;
+    $display ("ctrl_reg = %p", ctrl_reg);
+
+    // Assign a packed value to the structure variable
+    ctrl_reg = 8'hfa;
+    $display ("ctrl_reg = %p", ctrl_reg);
+  end
+endmodule
+
+/*
+ctrl_reg = '{mode:10, cfg:5, en:1}
+ctrl_reg = '{mode:3, cfg:5, en:1}
+ctrl_reg = '{mode:15, cfg:5, en:0}
+*/
---
+++ systemverilog/Data_Type/03_Structures/unpacked_struct.sv
@@ -0,0 +1,34 @@
+module tb;
+  	// Create a structure called "st_fruit"
+	// which to store the fruit's name, count and expiry date in days.
+  	// Note: this structure declaration can also be placed outside the module
+	typedef struct {
+  		string fruit;
+  		int    count;
+  		byte 	 expiry;
+	} st_fruit;
+
+  initial begin
+    // st_fruit is a data type, so we need to declare a variable of this data type
+    st_fruit fruit1 = '{"apple", 4, 15};
+    st_fruit fruit2;
+
+    // Display the structure variable
+    $display ("fruit1 = %p fruit2 = %p", fruit1, fruit2);
+
+    // Assign one structure variable to another and print
+    // Note that contents of this variable is copied into the other
+   	fruit2 = fruit1;
+    $display ("fruit1 = %p fruit2 = %p", fruit1, fruit2);
+
+    // Change fruit1 to see if fruit2 is affected
+    fruit1.fruit = "orange";
+    $display ("fruit1 = %p fruit2 = %p", fruit1, fruit2);
+  end
+endmodule
+
+/*
+fruit1 = '{fruit:"apple", count:4, expiry:15} fruit2 = '{fruit:"", count:0, expiry:0}
+fruit1 = '{fruit:"apple", count:4, expiry:15} fruit2 = '{fruit:"apple", count:4, expiry:15}
+fruit1 = '{fruit:"orange", count:4, expiry:15} fruit2 = '{fruit:"apple", count:4, expiry:15}
+*/
