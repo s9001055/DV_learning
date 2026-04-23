e5964634eec63eedbac4eb01feae59e15115eae6
Arthur Chang <s9001055@gmail.com>
2026-04-23 23:31:08 +0800

    [APB_TB] modify interface

--- tb_proj/APB_tb/UVM/apb_item.sv
+++ tb_proj/APB_tb/UVM/apb_item.sv
@@ -4,9 +4,13 @@
 class apb_item extends uvm_sequence_item;
     `uvm_object_utils(apb_item)
 
-    rand logic [APB_DATA_WIDTH-1:0] addr;
-    rand logic [APB_DATA_WIDTH-1:0] data;
-    rand logic                  is_write;
+    rand logic [APB_DATA_WIDTH-1:0]         paddr;
+    rand logic                              pwrite;
+    rand logic [APB_DATA_WIDTH-1:0]         pwdata;
+    rand logic [(APB_DATA_WIDTH/8)-1:0]     pstrb;
+         logic [APB_DATA_WIDTH-1:0]         prdata;   // 由 Monitor 填入，不需 rand
+         logic                              pslverr;
+
 
     function new(string name = "apb_item");
         super.new(name);
@@ -14,6 +18,7 @@ class apb_item extends uvm_sequence_item;
 
     // 格式化列印，方便 Debug
     function string convert2string();
-        return $sformatf("Addr=%0h, Data=%0h, Write=%0b", addr, data, is_write);
+        return $sformatf("[%s] ADDR=0x%08h DATA=0x%08h STRB=0x%h PRDATA=0x%08h ERR=%0b",
+            pwrite ? "WR" : "RD", paddr, pwdata, pstrb, prdata, pslverr);
     endfunction
 endclass
