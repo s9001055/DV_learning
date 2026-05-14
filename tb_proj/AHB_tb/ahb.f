c6d6b1aab5aa1b9def266f147fece0b56139d5bd
Arthur Chang <s9001055@gmail.com>
2026-05-14 22:56:57 +0800

    [AHB_TB] add ahb.f

---
+++ tb_proj/AHB_tb/ahb.f
@@ -0,0 +1,14 @@
+-uvm
+-sv
+-access +rwc
+./UVM/interface.sv
+./UVM/ahb_item.sv
+./UVM/ahb_driver.sv
+./UVM/ahb_monitor.sv
+./UVM/ahb_scoreboard.sv
+./UVM/ahb_agent.sv
+./UVM/ahb_env.sv
+./UVM/sequence.sv
+./UVM/base_test.sv
+./rtl/ahb_mem.v
+./UVM/top_tb.sv
