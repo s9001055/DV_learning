fa398a60e6e310edb73c3e788e3a529533da2bda
Arthur Chang <s9001055@gmail.com>
2026-08-12 23:08:45 +0800

    [AXI_VIP] add axi_fixed_rw_test

---
+++ tb_proj/AXI_VIP/test/axi_fixed_wr_test.sv
@@ -0,0 +1,18 @@
+`ifndef AXI_FIXED_RW_TEST_SV
+`define AXI_FIXED_RW_TEST_SV
+
+class axi_fixed_rw_test extends axi_base_test;
+    `uvm_component_utils(axi_fixed_rw_test)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    virtual task run_test_body();
+        seq = axi_fixed_wr_seq::type_id::create("seq");
+        seq.start(env.mst_agent.mst_sqr);
+    endtask
+
+endclass : axi_fixed_rw_test
+
+`endif
