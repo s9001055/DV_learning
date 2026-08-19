7f63b0d64a21a971377b0addeac9ccbf6419fa0c
Arthur Chang <s9001055@gmail.com>
2026-08-20 00:08:08 +0800

    [AXI_VIP] instance SVA module in top_tb

--- tb_proj/AXI_VIP/tb/tb_top.sv
+++ tb_proj/AXI_VIP/tb/tb_top.sv
@@ -26,6 +26,49 @@ module tb_top;
     // Interface
     axi_if #(.AWIDTH(32), .DWIDTH(64), .IDWIDTH(4)) axi_bus(aclk, aresetn);
 
+    // SVA module
+    axi_protocol_checker #(
+        .AWIDTH (32),
+        .DWIDTH (64),
+        .IDWIDTH(4)
+    ) u_axi_chk (
+        .aclk    (aclk),
+        .aresetn (aresetn),
+        // AW
+        .awid    (axi_bus.awid),
+        .awaddr  (axi_bus.awaddr),
+        .awlen   (axi_bus.awlen),
+        .awsize  (axi_bus.awsize),
+        .awburst (axi_bus.awburst),
+        .awvalid (axi_bus.awvalid),
+        .awready (axi_bus.awready),
+        // W
+        .wdata   (axi_bus.wdata),
+        .wlast   (axi_bus.wlast),
+        .wvalid  (axi_bus.wvalid),
+        .wready  (axi_bus.wready),
+        // B
+        .bid     (axi_bus.bid),
+        .bresp   (axi_bus.bresp),
+        .bvalid  (axi_bus.bvalid),
+        .bready  (axi_bus.bready),
+        // AR
+        .arid    (axi_bus.arid),
+        .araddr  (axi_bus.araddr),
+        .arlen   (axi_bus.arlen),
+        .arsize  (axi_bus.arsize),
+        .arburst (axi_bus.arburst),
+        .arvalid (axi_bus.arvalid),
+        .arready (axi_bus.arready),
+        // R
+        .rid     (axi_bus.rid),
+        .rresp   (axi_bus.rresp),
+        .rlast   (axi_bus.rlast),
+        .rvalid  (axi_bus.rvalid),
+        .rready  (axi_bus.rready)
+    );
+
+
     // self-loop 驗 VIP 
     initial begin
         uvm_config_db#(virtual axi_if)::set(null, "*", "vif", axi_bus);
