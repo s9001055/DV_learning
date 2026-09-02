8530f6e4247aff8afa57b8cadbc1f7206610b2c6
Arthur Chang <s9001055@gmail.com>
2026-09-03 01:31:11 +0800

    [AXI_VIP] remove slave agent sequencer

--- tb_proj/AXI_VIP/src/slave/axi_slv_agent.sv
+++ tb_proj/AXI_VIP/src/slave/axi_slv_agent.sv
@@ -5,7 +5,7 @@ class axi_slv_agent extends uvm_agent;
     `uvm_component_utils(axi_slv_agent)
 
     axi_slv_driver      slv_drv;
-    axi_sequencer       slv_sqr;
+    // axi_sequencer       slv_sqr;
     axi_slv_monitor     slv_mon;
     axi_reset_monitor   rst_mon;
 
@@ -19,7 +19,7 @@ class axi_slv_agent extends uvm_agent;
         slv_mon = axi_slv_monitor::type_id::create("slv_mon", this);
 
         if (get_is_active() == UVM_ACTIVE) begin
-            slv_sqr = axi_sequencer::type_id::create("slv_sqr", this);
+            // slv_sqr = axi_sequencer::type_id::create("slv_sqr", this);
             slv_drv = axi_slv_driver::type_id::create("slv_drv", this);
         end
     endfunction
@@ -27,9 +27,9 @@ class axi_slv_agent extends uvm_agent;
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
 
-        if (get_is_active() == UVM_ACTIVE) begin
-            slv_drv.seq_item_port.connect(slv_sqr.seq_item_export);
-        end
+        // if (get_is_active() == UVM_ACTIVE) begin
+        //     slv_drv.seq_item_port.connect(slv_sqr.seq_item_export);
+        // end
     endfunction
 
 endclass : axi_slv_agent
